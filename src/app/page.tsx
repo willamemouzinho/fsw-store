3301504834aae915b2419ebcefc194da20a3460a
Willame Mouzinho <willame.dev@gmail.com>
2023-10-18 23:25:37 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1 className='bg-slate-500 flex text-red-300'>Hello World</h1>
-  )
+  return <h1 className="">Hello World</h1>;
 }
