478d9a5b94966b2aaf5bd46a2cffb0b0c61dff88
Willame Mouzinho <willame.dev@gmail.com>
2023-10-20 20:23:22 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,57 @@
-import { CarTaxiFrontIcon, MenuIcon, ShoppingCart } from "lucide-react";
+import {
+  CarTaxiFrontIcon,
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCart,
+} from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between rounded-none p-8">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
-      <h1 className="text-lg font-semibold text-primary">Full Stack Week</h1>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <SheetHeader className="text-left font-bold">Menu</SheetHeader>
+
+          <div className="mt-8 flex flex-col gap-4">
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <LogInIcon size={16} />
+              Fazer Login
+            </Button>
+
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <HomeIcon size={16} />
+              Início
+            </Button>
+
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button className="w-full justify-start gap-2" variant="outline">
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">Full Stack Week</span> Store
+      </h1>
+
       <Button size="icon" variant="outline">
         <ShoppingCart />
       </Button>
