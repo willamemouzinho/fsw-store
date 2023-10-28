fa9f38cff8d6e9e99869f6c72e10e73b8d5de257
Willame Mouzinho <willame.dev@gmail.com>
2023-10-28 18:56:45 -0300

    feat: add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/header";
 import { AuthProvider } from "@/providers/auth";
+import Footer from "@/components/ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,7 @@ export default function RootLayout({
           <div className="flex h-full flex-col">
             <Header />
             <div className="flex-1">{children}</div>
-            <footer>Footer</footer>
+            <Footer />
           </div>
         </AuthProvider>
       </body>
---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-accent px-8 py-5 text-xs opacity-40">
+      © 2023 Copyright <span className="font-bold">FSW Store</span>
+    </footer>
+  );
+};
+
+export default Footer;
