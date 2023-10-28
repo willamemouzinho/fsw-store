a859c458511728e86b326f31efe32e9f1a1c79dd
Willame Mouzinho <willame.dev@gmail.com>
2023-10-28 16:00:38 -0300

    feat: add Categories and CategoryItem components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   return (
-    <div className="px-8">
+    <div className="">
       <Image
         src="/banner-home-01.png"
         height={0}
         width={0}
         alt="Atè 55% de desconto só esse mês"
         sizes="100vw"
-        className="h-auto w-full"
+        className="h-auto w-full px-8"
       />
+      <div className="mt-8 px-8">
+        <Categories />
+      </div>
     </div>
   );
 }
