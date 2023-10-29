d71177e512cb16e6659f41ddb9b3f7260e4cbb79
Willame Mouzinho <willame.dev@gmail.com>
2023-10-29 13:03:47 -0300

    feat: ensure user goes to catalog page and home page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -12,9 +12,16 @@ import {
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 const Header = () => {
   const { status, data } = useSession();
@@ -80,27 +87,43 @@ const Header = () => {
               </Button>
             )}
 
-            <Button className="w-full justify-start gap-2" variant="outline">
-              <HomeIcon size={16} />
-              Início
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  className="w-full justify-start gap-2"
+                  variant="outline"
+                >
+                  <HomeIcon size={16} />
+                  Início
+                </Button>
+              </Link>
+            </SheetClose>
 
             <Button className="w-full justify-start gap-2" variant="outline">
               <PercentIcon size={16} />
               Ofertas
             </Button>
 
-            <Button className="w-full justify-start gap-2" variant="outline">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button
+                  className="w-full justify-start gap-2"
+                  variant="outline"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
 
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">Full Stack Week</span> Store
-      </h1>
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">Full Stack Week</span> Store
+        </h1>
+      </Link>
 
       <Button size="icon" variant="outline">
         <ShoppingCart />
