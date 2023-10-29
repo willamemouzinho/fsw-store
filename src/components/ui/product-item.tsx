d8a4e486581afc94cff37283056194a64203c173
Willame Mouzinho <willame.dev@gmail.com>
2023-10-29 13:25:09 -0300

    fix: ProductItem style updated

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -8,7 +8,7 @@ interface ProductItemProps {
 }
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex flex-col gap-4 rounded-lg">
+    <div className="flex min-w-[156px] flex-col gap-4 rounded-lg">
       <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
