bb8c7c9023c0985ddf59a83d9f50ed683249459e
Willame Mouzinho <willame.dev@gmail.com>
2023-11-03 22:24:58 -0300

    style: change Footer component bg-color

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-accent px-8 py-5 text-xs text-zinc-500">
+    <footer className="text-slate-500-500 bg-slate-900 px-8 py-5 text-xs">
       © 2023 Copyright <span className="font-bold">FSW Store</span>
     </footer>
   );
